4134189fc20cd435d6f1afcd185d0c94ebd5fb5c
Adin Gilman-Cohen <adingilmancohen@students.berkeley.net>
2018-03-26 16:38:48 -0700

    asdf

--- script.js
+++ script.js
@@ -7,53 +7,80 @@ var wordBankExplicit2=["thicc","peeker","Cock block","Masterdate","squashing","M
 
 var num=0
 
+var incorrectGuesses=0;
+var correctGuesses=0;
+
+
 
 
 $(document).ready(function (){
-    $('#newDef').on('click',function() {
-        chooseWord();
-        dropDown();
-
-        $.ajax({
-            url: "https://mashape-community-urban-dictionary.p.mashape.com/define?term=" + word,
-            type: 'GET',
-            beforeSend: function (x) {
-                x.setRequestHeader('X-Mashape-Key', "peVVIuMiKcmshoAindSsWWyDE2OEp1X64xsjsn8wGjJgn2Yt5d")
-            },
-            crossDomain: true,
-            dataType: 'json',
-            success: function (result) {
-                console.log(result);
-                getDef(result);
-
-            },
-            error: function () {
-                alert('Failed!');
-            }
-        });
+    $('#launch').on('click',function() {
+        incorrectGuesses=0;
+        correctGuesses=0;
+        $('#correctGuesses').html(correctGuesses);
+        $('#incorrectGuesses').html(incorrectGuesses);
+       all()
 
     });
     //PROCESS guess
     $('#submitGuess').on('click',function() {
         var wordGuess=$('#wordOptions').val();
-        var guesses=0
+        var processedguess= arr2[wordGuess];
+
+
         $('#winOrLose').html('');
 
-        console.log(wordGuess);
-        if(wordGuess==num){
-            console.log("sucess");
+        console.log(processedguess);
+        console.log(num);
+        console.log(arr1[num]);
+        if(processedguess==arr1[num]){
             $('#winOrLose').append('That is correct!');
+            correctGuesses+=1;
+            arr1.splice(num,1);
+            all();
         }else{
             $('#winOrLose').append('That is incorrect');
-            guesses+=1
-            $('#guesses').append(guesses);
+            incorrectGuesses+=1;
+
+
 
 
         }
-        
+        $('#correctGuesses').html(correctGuesses);
+        $('#incorrectGuesses').html(incorrectGuesses);
+    });
+});
+
+
+function all(){
+    chooseWord();
+    dropDown();
+
+    $.ajax({
+        url: "https://mashape-community-urban-dictionary.p.mashape.com/define?term=" + word,
+        type: 'GET',
+        beforeSend: function (x) {
+            x.setRequestHeader('X-Mashape-Key', "peVVIuMiKcmshoAindSsWWyDE2OEp1X64xsjsn8wGjJgn2Yt5d")
+        },
+        crossDomain: true,
+        dataType: 'json',
+        success: function (result) {
+            console.log(result);
+            getDef(result);
+            // getDef(result);
+
+        },
+        error: function () {
+            alert('Failed!');
+        }
     });
 
-});
+
+
+}
+
+
+
 
 //chooses word from clean or dirty array
 function chooseWord(){
@@ -69,7 +96,6 @@ function chooseWord(){
     word=arr1[num];
     console.log(word);
     console.log(arr1);
-    arr1.splice(num,1);
     console.log(arr1);
 
 }
